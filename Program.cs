12fb1f8586c8808a418680976dfa90582f557885
lis0jk <64403491+lis0jk@users.noreply.github.com>
2020-05-19 13:07:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _4._2
+{
+    class Program
+    {
+        [DllImport("L4DLL.dll")]
+        public static extern int Count(int a, int b);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("а = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b = ");
+            int b = Convert.ToInt32(Console.ReadLine());            
+            int r = Count(a, b);
+            Console.WriteLine("r = {0}", r);
+        }
+    }
+}
---
+++ dllmain.cpp
@@ -0,0 +1,10 @@
+﻿#include "pch.h"
+
+extern "C"
+{
+	__declspec(dllexport) int Count(int a, int b)
+	{
+		int rez = a + b;
+		return rez;
+	}
+}
